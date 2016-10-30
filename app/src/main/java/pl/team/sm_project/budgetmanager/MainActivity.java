4b3e28a214f6a8c4affd061762d80d0281776f61
Saibamen <saibamenppl@gmail.com>
2016-10-30 21:36:13 +0100

    Git Init

---
+++ app/src/main/java/pl/team/sm_project/budgetmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.team.sm_project.budgetmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
