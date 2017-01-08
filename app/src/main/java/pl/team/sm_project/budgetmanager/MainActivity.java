59d4de6ec439a5fdd859bd7104df3e71517a9b59
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2017-01-08 01:16:29 +0100

    Aktywacja Crashlyrics

--- app/src/main/java/pl/team/sm_project/budgetmanager/MainActivity.java
+++ app/src/main/java/pl/team/sm_project/budgetmanager/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
