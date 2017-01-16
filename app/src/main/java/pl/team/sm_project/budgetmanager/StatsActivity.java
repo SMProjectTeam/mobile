ca8162df05225514c92ef3a3d33046990fd6eef0
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2017-01-16 02:57:26 +0100

    Drugi wykres

--- app/src/main/java/pl/team/sm_project/budgetmanager/StatsActivity.java
+++ app/src/main/java/pl/team/sm_project/budgetmanager/StatsActivity.java
@@ -31,13 +31,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class StatsActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
     private String json_string;
 
-    private PieChart mChart;
+    private PieChart mChart, mChart2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,54 +52,65 @@ public class StatsActivity extends AppCompatActivity implements OnChartValueSele
         //getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mChart = (PieChart) findViewById(R.id.chart1);
-        mChart.setUsePercentValues(true);
-        mChart.getDescription().setEnabled(false);
-        mChart.setExtraOffsets(5, 10, 5, 5);
+        mChart2 = (PieChart) findViewById(R.id.chart2);
 
-        mChart.setDragDecelerationFrictionCoef(0.95f);
-
-        mChart.setDrawHoleEnabled(true);
-        mChart.setHoleColor(Color.WHITE);
-
-        mChart.setTransparentCircleColor(Color.WHITE);
-        mChart.setTransparentCircleAlpha(110);
-
-        mChart.setHoleRadius(58f);
-        mChart.setTransparentCircleRadius(61f);
-
-        mChart.setDrawCenterText(true);
-
-        mChart.setRotationAngle(0);
-        // enable rotation of the chart by touch
-        mChart.setRotationEnabled(true);
-        mChart.setHighlightPerTapEnabled(true);
-
-        // add a selection listener
-        mChart.setOnChartValueSelectedListener(this);
+        setupChart(mChart);
+        setupChart(mChart2);
 
         getJSON();
+    }
 
-        mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+    private void setupChart(PieChart chart) {
+        chart.setUsePercentValues(true);
+        chart.getDescription().setEnabled(false);
+        chart.setExtraOffsets(5, 10, 5, 5);
 
-        Legend l = mChart.getLegend();
+        chart.setDragDecelerationFrictionCoef(0.95f);
+
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.WHITE);
+
+        chart.setTransparentCircleColor(Color.WHITE);
+        chart.setTransparentCircleAlpha(110);
+
+        chart.setHoleRadius(58f);
+        chart.setTransparentCircleRadius(61f);
+
+        chart.setDrawCenterText(true);
+
+        chart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        chart.setRotationEnabled(true);
+        chart.setHighlightPerTapEnabled(true);
+
+        // add a selection listener
+        chart.setOnChartValueSelectedListener(this);
+
+        // getJSON
+
+        chart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+        Legend l = chart.getLegend();
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        l.setDrawInside(false);
+        l.setDrawInside(true);
         l.setXEntrySpace(7f);
         l.setYEntrySpace(0f);
         l.setYOffset(60f);
         l.setTextSize(18f);
+        l.setFormLineWidth(20f);
+        l.setWordWrapEnabled(true);
         l.setTextColor(Color.CYAN);
 
         // entry label styling
-        mChart.setEntryLabelColor(Color.MAGENTA);
-        mChart.setEntryLabelTextSize(16f);
+        chart.setEntryLabelColor(Color.MAGENTA);
+        chart.setEntryLabelTextSize(16f);
     }
 
     private SpannableString generateCenterSpannableText(String title) {
         SpannableString s = new SpannableString("Budget Manager\n" + title);
-        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+        s.setSpan(new RelativeSizeSpan(1.3f), 0, 14, 0);
 
         return s;
     }
@@ -148,12 +160,58 @@ public class StatsActivity extends AppCompatActivity implements OnChartValueSele
 
     private void showStats() {
         JSONObject json_object;
+        JSONArray json_array;
         ArrayList<PieEntry> entries = new ArrayList<>();
+        ArrayList<ArrayList<PieEntry>> entriess = new ArrayList<>();
+
+        Log.i("BM", "json_stringgg: " + json_string);
 
         try {
-            json_object = new JSONObject(json_string);
+            json_array = new JSONArray(json_string);
+
+            Log.i("BM", "json_array: " + json_array);
+            Log.i("BM", "json_array.length(): " + json_array.length());
+
+            for (int i = 0; i < json_array.length(); i++) {
+                JSONObject stat = json_array.getJSONObject(i);
+
+                Log.i("BM", "JSON stat in for i loop: " + stat);
+
+                JSONArray data = stat.getJSONArray("js_chart_data");
+
+                Log.i("BM", "js_chart_data: " + data);
+                Log.i("BM", "js_chart_data.length(): " + data.length());
+
+                entriess.add(new ArrayList<PieEntry>());
+
+                for (int j = 0; j < data.length(); j++) {
+                    Log.i("BM", "data.get(j): " + data.get(j));
+                    Log.i("BM", "data.get(j).toString().length(): " + data.get(j).toString().length());
+                    Log.i("BM", "data.optJSONArray(j).length(): " + data.optJSONArray(j).length());
+
+                    Log.i("BM", "data.optJSONArray(j).get(0): " + data.optJSONArray(j).get(0));
+                    Log.i("BM", "data.optJSONArray(j).get(1): " + data.optJSONArray(j).get(1));
+
+                    entriess.get(i).add(new PieEntry((float) data.optJSONArray(j).getDouble(1), data.optJSONArray(j).getString(0)));
+
+                    //entriess.get(i).add(new PieEntry((float) data.optJSONArray(j).getDouble(1), data.optJSONArray(j).getString(0)));
+
+                    //entries.add(new PieEntry((float) data.optJSONArray(j).getDouble(1), data.optJSONArray(j).getString(0)));
+                }
+
+                //entries.add(new PieEntry((float) data, ((String) data)));
+            }
+
+            Log.i("BM", "entriess: " + entriess);
+
+            /*json_object = new JSONObject(json_string);
+
+            Log.i("BM", "json_object: " + json_object);
+            Log.i("BM", "JSON_object.length(): " + json_object.length());
 
             JSONArray result = json_object.getJSONArray(WebConfig.TAG_JSON_ARRAY);
+
+            Log.i("BM", "JSONArray result.length(): " + result.length());
 
             for (int i = 0; i < result.length(); i++) {
                 JSONObject stat = result.getJSONObject(i);
@@ -163,12 +221,13 @@ public class StatsActivity extends AppCompatActivity implements OnChartValueSele
                 Object name = stat.getJSONObject("user").get("name");
 
                 entries.add(new PieEntry((float) stat.getDouble("sum"), ((String) name)));
-            }
+            }*/
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        setData(mChart, entries, getString(R.string.expenditures_by_users));
+        setData(mChart, entriess.get(0), getString(R.string.expenditures_by_users));
+        setData(mChart2, entriess.get(1), getString(R.string.expenditures_by_sources));
     }
 
     private void setData(PieChart chart, ArrayList<PieEntry> entries, String title) {
